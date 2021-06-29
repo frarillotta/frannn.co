ce6a9c163d0cd69d9aef13f2508c07b4bc97fff9
frarillotta <frarillotta@gmail.com>
2021-06-29 19:09:57 +0100

    empty commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,7 +11,7 @@ export default class MyDocument extends Document {
         originalRenderPage({
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
-        })
+        }) 
 
       const initialProps = await Document.getInitialProps(ctx)
       return {
