770ef0bb449e54f0084a63827b35c798df1dd06e
Francesco Arillotta <francesco.arillotta@thetrainline.com>
2023-09-02 15:52:38 +0100

    try this

--- src/app/shaders/DitherTexture/page.tsx
+++ src/app/shaders/DitherTexture/page.tsx
@@ -3,18 +3,22 @@
 import dynamic from "next/dynamic"
 import { GithubIcon } from '@/components/GithubIcon/GithubIcon';
 import styled from "styled-components";
+import fragmentShader from '@/components/shaders/ditherTexture';
+import { ShaderBase } from "@/components/3d/Shaders/ShaderBase";
 
-const DitherTexture = dynamic(() => import('@/components/3d/Shaders/DitherTexture/DitherTexture').then((mod) => mod.DitherTexture), {
+const Canvas = dynamic(() => import('@/components/canvas/Canvas').then((mod) => mod.Canvas), {
     ssr: false
 })
+
 export default () => {
     return <>
-        <DitherTexture />
+        <Canvas>
+            <ShaderBase fragmentShader={fragmentShader(false)} texture={'/anseladams.jpg'} />
+        </Canvas>
         <GithubWrapper href="https://github.com/frarillotta/frannn.co/blob/master/src/components/3d/Shaders/DitherTexture/worker.tsx#L6" target="_blank">
             <GithubIcon />
         </GithubWrapper>
     </>
-
 }
 
 const GithubWrapper = styled.a`
--- src/app/shaders/VolumeLight/page.tsx
+++ src/app/shaders/VolumeLight/page.tsx
@@ -3,13 +3,18 @@
 import dynamic from "next/dynamic"
 import { GithubIcon } from '@/components/GithubIcon/GithubIcon';
 import styled from "styled-components";
+import fragmentShader from '@/components/shaders/volumeLight';
+import { ShaderBase } from "@/components/3d/Shaders/ShaderBase";
 
-const VolumeLight = dynamic(() => import('@/components/3d/Shaders/VolumeLight/VolumeLight').then((mod) => mod.VolumeLight), {
+const Canvas = dynamic(() => import('@/components/canvas/Canvas').then((mod) => mod.Canvas), {
     ssr: false
 })
+
 export default () => {
     return <>
-        <VolumeLight />
+        <Canvas>
+            <ShaderBase fragmentShader={fragmentShader} />
+        </Canvas>
         <GithubWrapper  href="https://github.com/frarillotta/frannn.co/blob/master/src/components/3d/Shaders/VolumeLight/worker.tsx#L6"  target="_blank">
             <GithubIcon />
         </GithubWrapper>
